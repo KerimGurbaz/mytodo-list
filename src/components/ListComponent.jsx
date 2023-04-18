1377b544be2d9cc74ceda227d5b4829297132d01
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-04-18 16:34:03 +0200

    list component v-2

--- src/components/ListComponent.jsx
+++ src/components/ListComponent.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 
-const ListComponent = ({ tasks, deleteTask }) => {
+const ListComponent = ({ tasks, deleteTask, editTask, toggleCopmlete }) => {
   return (
     <ul>
       {tasks.map((task, index) => (
-        <li>
-          <span>{task.text}</span>
-          <button onClick={() => deleteTask(index)}>Delete</button>
+        <li key={index} className={task.isCompleted ? "completed" : ""}>
+          {task.isEditing ? (
+            <input
+              value={task.text}
+              onChange={(e) => editTask(index, e.target.value)}
+            />
+          ) : (
+            <span onClick={() => toggleCopmlete(index)}>{task.text}</span>
+          )}
+          <button onClick={() => deleteTask(index)}>Sil</button>
+          {task.isEditing ? (
+            <button onClick={() => editTask(index)}>Kaydet</button>
+          ) : (
+            <button onClick={() => editTask(index, task.text)}>Düzenle</button>
+          )}
         </li>
       ))}
     </ul>
